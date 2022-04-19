70af4835095a8209203e04e8ec4ce92de0b53a99
JiggyboyK <korede305@gmail.com>
2022-04-19 13:01:42 +0100

    style change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import imgInSrc from './imageInSrc.jpg'
 
-import src from './imageInSrc.jpg'
 
 function App() {
   return (
     <div className="App">
     
       <div>
-      <h1 clsss="title blue">bruce WAYNE</h1>
+      <h1 clsss="title blue" style={{color: 'red'}}> GOTHAM CITY</h1>
+      <h2> Joker x Batman</h2>
 
         <img src={process.env.PUBLIC_URL + '/imageInPublic.jpg'} alt="" />
+        <img src={imgInSrc} />
       </div>
 
       <video width="320" height= "240" controls>
